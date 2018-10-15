92f2a3290b177eda256d2fb75b6dfad3d66e61cc
Satyasri Varaganti <satyasrib@gmail.com>
2018-10-15 19:04:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,12 +17,13 @@ const two = 2
 
 }
 
-var funkyFunction = function() {
+var funkyFunction = function theFunk() {
   return function() {
+
     return "FUNKY!"
   }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = FUNKY!
+var theFunk = funkyFunction()
